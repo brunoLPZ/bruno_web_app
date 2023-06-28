243a306448b0bd04abfb514cab62b36adb61ac4f
Bruno López <bruno.lopez.trigo@gmail.com>
2023-06-28 23:00:34 +0200

    Missing changes

--- src/App.js
+++ src/App.js
@@ -1,156 +1,53 @@
-import './App.css';
-import technologyVideo from './assets/technology.mp4';
-import keyboard from './assets/keyboard.jpg';
-
-import Cover from './cover/Cover';
-import Experience from './experience/Experience';
-import { useState } from 'react';
-import * as Unicons from '@iconscout/react-unicons';
-import { Parallax, Background } from 'react-parallax';
-import { useInView } from 'react-intersection-observer';
-import Certifications from './certifications/Certifications';
-import Contact from './contact/Contact';
+import Home from "./pages/Home";
+import Cover from "./pages/Cover";
+import Experience from "./pages/Experience";
+import ExperienceHeading from "./pages/ExperienceHeading";
+import Hacking from "./pages/Hacking";
+import citius from './assets/citius.svg';
+import imatia from './assets/imatia.png';
+import ttech from './assets/ttech.svg';
+import Contact from "./pages/Contact";
 
 function App() {
-  const [theme, setTheme] = useState('light');
-  const [refHome, homeActive, homeEntry] = useInView({
-    /* Optional options */
-    threshold: 0.5,
-  });
-  const [refExperience, experienceActive, experienceEntry] = useInView({
-    /* Optional options */
-    threshold: 0.5,
-  });
-
-  const [refCertifications, certificationsActive, certificationsEntry] =
-    useInView({
-      /* Optional options */
-      threshold: 0.5,
-    });
-
-  const [refContact, contactActive, contactEntry] = useInView({
-    /* Optional options */
-    threshold: 0.5,
-  });
-
-  const scrollToSection = (ref) => {
-    ref.target.scrollIntoView({ behavior: 'smooth' });
-  };
-
+    const experiences = [
+        {
+            year: 2018,
+            logo: citius,
+            position: 'Senior Research Support Technician',
+            explanation: 'Develop <span class="underline text-emerald-600">ExpliClas</span>.\n' +
+                '                    This was part of my degree final project.\n' +
+                '                    In a few words. This was a Java REST API with a React frontend. The goal was to generate natural\n' +
+                '                    language explanations to machine learning algorithms.'
+        },
+        {
+            year: 2019,
+            logo: imatia,
+            position: 'Fullstack Developer',
+            explanation: 'Worked on two big projects here. A ' +
+                '<span class="underline text-emerald-600">microservice-oriented architecture</span> and a ' +
+                '<span class="underline text-emerald-600">serverless</span> ' +
+                'architecture was what I came across. SpringBoot, Angular, Docker, RabbitMQ are just some examples of ' +
+                'technologies that I used.'
+        },
+        {
+            year: 2022,
+            logo: ttech,
+            position: 'Fullstack Developer / Innovation Analyst',
+            explanation: 'Provide value to the company by developing innovative solutions in ' +
+                '<span class="underline text-emerald-600">cloud</span> and ' +
+                '<span class="underline text-emerald-600">cyber-security</span> areas. ' +
+                'SpringBoot, Angular and Python as main technology stack. First approaches to solutions involving ' +
+                'generative AI algorithms like LLMs.'
+        }
+    ]
   return (
-    <div id="app" className={theme}>
-      <Parallax strength={500}>
-        <Background className="custom-bg">
-          <div
-            style={{
-              display: 'flex',
-              alignItems: 'center',
-              height: '100vh',
-            }}
-          >
-            <video
-              src={technologyVideo}
-              autoPlay
-              muted
-              loop
-              style={{
-                height: '70%',
-                filter:
-                  theme === 'dark'
-                    ? 'grayscale(1) invert(15%) blur(1px)'
-                    : 'grayscale(1) invert(80%) blur(1px)',
-              }}
-            ></video>
-          </div>
-        </Background>
-        <div className="app-page" ref={refHome}>
-          <Cover></Cover>
-        </div>
-      </Parallax>
-      <Parallax
-        strength={500}
-        renderLayer={(percentage) => (
-          <div className="app-page" ref={refExperience}>
-            <Experience
-              parallaxPercentage={percentage}
-              theme={theme}
-            ></Experience>
-          </div>
-        )}
-      ></Parallax>
-      <Parallax
-        strength={500}
-        bgImage={keyboard}
-        bgClassName={`app-certificate-bg ${theme}`}
-        renderLayer={(percentage) => (
-          <div className="app-page" ref={refCertifications}>
-            <Certifications
-              parallaxPercentage={percentage}
-              theme={theme}
-            ></Certifications>
-          </div>
-        )}
-      ></Parallax>
-      <Parallax
-        strength={500}
-        renderLayer={(percentage) => (
-          <div className="app-page" ref={refContact}>
-            <Contact parallaxPercentage={percentage} theme={theme}></Contact>
-          </div>
-        )}
-      ></Parallax>
-      <div className={`app-navbar ${theme}`}>
-        <div
-          className={`app-navbar-item ${homeActive ? 'active' : ''}`}
-          onClick={() => scrollToSection(homeEntry)}
-        >
-          <div className={`circle circle-${theme}`}></div>
-          <span>Home</span>
-        </div>
-        <div
-          className={`app-navbar-item ${experienceActive ? 'active' : ''}`}
-          onClick={() => scrollToSection(experienceEntry)}
-        >
-          <div className={`circle circle-${theme}`}></div>
-          <span>Experience</span>
-        </div>
-        <div
-          className={`app-navbar-item ${certificationsActive ? 'active' : ''}`}
-          onClick={() => scrollToSection(certificationsEntry)}
-        >
-          <div className={`circle circle-${theme}`}></div>
-          <span>Certifications</span>
-        </div>
-        <div
-          className={`app-navbar-item ${contactActive ? 'active' : ''}`}
-          onClick={() => scrollToSection(contactEntry)}
-        >
-          <div className={`circle circle-${theme}`}></div>
-          <span>Contact</span>
-        </div>
-      </div>
-      <div
-        className={'app-theme-btn animate-appear-right-2x ' + theme}
-        onClick={() => setTheme(theme === 'dark' ? 'light' : 'dark')}
-      >
-        <Unicons.UilSun
-          size={30}
-          color={theme === 'light' ? '#dac000' : '#222222'}
-        />
-        <Unicons.UilMoon
-          size={30}
-          color={theme === 'dark' ? '#7f7fa3 ' : '#f9f7ee'}
-        />
-        <div
-          className="cloud"
-          style={{ top: theme === 'light' ? '38px' : '5px' }}
-        >
-          <Unicons.UilCloud
-            size={30}
-            color={theme === 'light' ? '#222222' : '#f9f7ee'}
-          />
-        </div>
-      </div>
+    <div className="bg-black no-scrollbar h-screen overflow-auto overflow-x-hidden snap-y snap-mandatory">
+        <Home></Home>
+        <Cover></Cover>
+        <ExperienceHeading></ExperienceHeading>
+        {experiences.map((exp, idx) => <Experience key={idx} experience={exp}></Experience>)}
+        <Hacking></Hacking>
+        <Contact></Contact>
     </div>
   );
 }
